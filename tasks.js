8cb46703a8fb7c344a5cc4c6bf077f5e81ac2e86
Jannik Zschiesche <jz@becklyn.com>
2015-03-13 03:03:26 +0100

    Renamed sass task file to "scss" and ordered tasks according to the docs

--- tasks.js
+++ tasks.js
@@ -4,8 +4,8 @@
 module.exports = {
     js:        require("./tasks/js_bundle"),
     js_simple: require("./tasks/js_simple"),
-    scss:      require("./tasks/sass"),
-    layout:    require("./tasks/layout"),
+    scss:      require("./tasks/scss"),
     jshint:    require("./tasks/jshint"),
+    layout:    require("./tasks/layout"),
     publish:   require("./tasks/publish")
 };
